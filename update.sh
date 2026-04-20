b2d55d96fa773ccbcf08842e7d56f7615f0aca15
chengran <chengran@archeros.com>
2026-04-20 13:47:08 +0800

    11

--- update.sh
+++ update.sh
@@ -6,6 +6,7 @@ timeout 5 bash "$(dirname "$0")/clash.meta/ip_Update/ip_3.sh"
 timeout 5 bash "$(dirname "$0")/clash.meta/ip_Update/ip_4.sh"
 timeout 5 bash "$(dirname "$0")/clash.meta/ip_Update/ip_5.sh"
 timeout 5 bash "$(dirname "$0")/clash.meta/ip_Update/ip_6.sh"
+timeout 5 wget -t 2 --no-check-certificate https://www.gitlabip.xyz/Alvin9999/PAC/refs/heads/master/backup/img/1/2/ipp/singbox/1/config.json
 
 
 git add *
